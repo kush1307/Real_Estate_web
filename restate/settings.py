433bbf5998209ce2be79cad79d5aeb2bbf230be5
Kush Patel <kushpatel.inexture@gmail.com>
2021-04-16 23:36:40 +0530

    email and dashboard

--- restate/settings.py
+++ restate/settings.py
@@ -141,3 +141,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'kushkushalpatel@gmail.com'
+EMAIL_HOST_PASSWORD = 'kushkushal'
+EMAIL_USE_TLS = True
