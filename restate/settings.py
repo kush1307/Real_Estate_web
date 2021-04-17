8632ef37afa6f71f7f37ac9f5bb21b332dc2091a
Kush Patel <kushpatel.inexture@gmail.com>
2021-04-17 08:57:07 +0530

    removed email

--- restate/settings.py
+++ restate/settings.py
@@ -143,9 +143,3 @@ MESSAGE_TAGS = {
 }
 
 # Email Config
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kushkushalpatel@gmail.com'
-EMAIL_HOST_PASSWORD = 'kushkushal'
-EMAIL_USE_TLS = True
